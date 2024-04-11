fd127045aaeee3703b966c883f15e1fe86430d50
jotpalch <jotp076315217@gmail.com>
2024-04-11 15:27:15 +0800

    Update: Refactor moss_check.py

--- moss_check.py
+++ moss_check.py
@@ -11,14 +11,14 @@ def main():
     # Get the user id from the command line arguments
     userid = sys.argv[1]
 
-    # Initialize the Moss object with the user id and language
-    m = mosspy.Moss(userid, "cc")
-
     # Define the directory where the C++ files are stored
     code_directory = "./code_backup/"
 
     # Iterate over the directories in the code directory
     for problem_dir in sorted(os.listdir(code_directory)):
+        # Initialize the Moss object with the user id and language
+        m = mosspy.Moss(userid, "cc")
+
         problem_path = os.path.join(code_directory, problem_dir)
 
         # Add each C++ file in the current directory to the Moss object
@@ -27,8 +27,7 @@ def main():
                 file_path = os.path.join(problem_path, file)
                 try:
                     m.addFile(file_path)
-                except Exception as e:
-                    print(f"Failed to add file {file_path} due to error: {str(e)}")
+                except:
                     continue
 
         # Send the files to Moss and get the report URL
